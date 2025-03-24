b7717b95da5cb628aa6bf8095d0d65931b357bb1
lwmacct <lwmacct@icloud.com>
2025-03-24 22:21:41 +0800

    -

--- pkg/mtsm/ts_handler.go
+++ pkg/mtsm/ts_handler.go
@@ -20,22 +20,20 @@ func NewHandler(opts ...handlerOpts) *TsHandler {
 	return t
 }
 
-// WithHandlerVector 设置 Vector 数据
-func WithHandlerVector(vector []TsVector) handlerOpts {
-	return func(t *TsHandler) {
-		t.Vector = vector
-	}
+// setVector 设置 Vector 数据
+func (t *TsHandler) SetVector(vector []TsVector) *TsHandler {
+	t.Vector = vector
+	return t
 }
 
-// WithHandlerMatrix 设置 Matrix 数据
-func WithHandlerMatrix(matrix []TsMatrix) handlerOpts {
-	return func(t *TsHandler) {
-		t.Matrix = matrix
-	}
+// setMatrix 设置 Matrix 数据
+func (t *TsHandler) SetMatrix(matrix []TsMatrix) *TsHandler {
+	t.Matrix = matrix
+	return t
 }
 
-// ToTv 将 Matrix 和 Vector 的 Value 字段转换为 Valuet 和 Valuev 切片
-func (t *TsHandler) ToTv() *TsHandler {
+// ExecToTv 将 Matrix 和 Vector 的 Value 字段转换为 Valuet 和 Valuev 切片
+func (t *TsHandler) ExecToTv() *TsHandler {
 	for i := range t.Matrix {
 		t.Matrix[i].ValueToTv()
 	}
@@ -45,24 +43,24 @@ func (t *TsHandler) ToTv() *TsHandler {
 	return t
 }
 
-// MergeGlobalLabel 将前缀 "g_" 标签加入到全局, 并移除 __name__ 标签
-func (t *TsHandler) MergeGlobalLabel() *TsHandler {
+// ExecMergeGlobalLabel 将前缀 "g_" 标签加入到全局, 并移除 __name__ 标签
+func (t *TsHandler) ExecMergeGlobalLabel() *TsHandler {
 	for i := range t.Vector {
 		t.Metrics = t.Vector[i].Metric["__name__"]
-		t.Vector[i].Metric = t.mergeGlobalLabel(t.Vector[i].Metric)
+		t.Vector[i].Metric = t.execMergeGlobalLabel(t.Vector[i].Metric)
 	}
 
 	for i := range t.Matrix {
 		if i == 0 {
 			t.Metrics = t.Matrix[i].Metric["__name__"]
 		}
-		t.Matrix[i].Metric = t.mergeGlobalLabel(t.Matrix[i].Metric)
+		t.Matrix[i].Metric = t.execMergeGlobalLabel(t.Matrix[i].Metric)
 	}
 	return t
 }
 
-// 将前缀 "g_" 标签加入到全局, 并移除 __name__ 标签
-func (t *TsHandler) mergeGlobalLabel(maps map[string]string) map[string]string {
+// execMergeGlobalLabel 将前缀 "g_" 标签加入到全局, 并移除 __name__ 标签
+func (t *TsHandler) execMergeGlobalLabel(maps map[string]string) map[string]string {
 	findName := false
 	for key, value := range maps {
 		// 如果是 __name__ 标签, 则移除
